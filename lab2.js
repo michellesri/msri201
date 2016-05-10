714e1732bdb7185ad1a2a378d348621119cd39b0
Michelle Sri <michelle.sri@gmail.com>
2016-05-10 16:17:53 -0700

    lab2

---
+++ lab2.js
@@ -0,0 +1,60 @@
+var f = document.getElementById("first");
+var s = document.getElementById("second");
+var t = document.getElementById("third");
+
+var userName = prompt("what is your name?");
+alert("hello " + userName + " i have some yes/no questions for you");
+
+var keepAsking = true;
+while (keepAsking) {
+  var firstAnswer = prompt("do i have a bachelor's degree?");
+  if (firstAnswer.toLowerCase() == "yes") {
+    alert("correct!");
+    keepAsking = false;
+    f.textContent = "degree " + firstAnswer + " woohoo bachelor's!"
+  }
+  else if (firstAnswer.toLowerCase() == "no") {
+    alert("that's incorrect. i do have a BA");
+    keepAsking = false;
+  }
+  else {
+    alert("not a valid answer");
+    keepAsking = true;
+  }
+}
+
+var secondAsking = true;
+while (secondAsking) {
+  var secondAsking = prompt("am i a fun person who likes adventure?");
+  if (secondAsking.toLowerCase() == "yes") {
+    alert("correct!");
+    secondAsking = false;
+    s.textContent = "michelle is fun and adventurous!"
+  }
+  else if (secondAsking.toLowerCase() == "no") {
+    alert("that's incorrect. i recently adventured to iceland and thailand");
+    secondAsking = false;
+  }
+  else {
+    alert("not a valid answer");
+    secondAsking = true;
+  }
+}
+
+var thirdAsking = true;
+while (thirdAsking) {
+  var thirdAsking = prompt("am i a great person to hire?");
+  if (thirdAsking.toLowerCase() == "yes") {
+    alert("correct! i am great at teamwork!");
+    thirdAsking = false;
+    t.textContent = "michelle is outgoing and helps new team members integrate to their new teams!"
+  }
+  else if (thirdAsking.toLowerCase() == "no") {
+    alert("that's incorrect. playing basketball and dancing my whole life have taught me how to work in teams.");
+    thirdAsking = false;
+  }
+  else {
+    alert("not a valid answer");
+    thirdAsking = true;
+  }
+}
