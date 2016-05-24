fc8e97772d2718477ebd5afb421a7ca05a7c0fe5
Michelle Sri <michelle.sri@gmail.com>
2016-05-23 22:35:25 -0700

    added three images selecting randomly

--- week2/lab11/app.js
+++ week2/lab11/app.js
@@ -1,15 +1,88 @@
-//http://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb the code on this link is the outline for switchPic
-var totalClicks = 0;
-
-function switchPic() {
-  console.log('switchPic called');
-  totalClicks++;
-  console.log(totalClicks);
-  var image = document.getElementById('pic1');
-  if (image.src.match('jon')){
-    image.src = image.src = 'sf.jpg';
-  }
-  else {
-    image.src = 'jon.jpg';
-  }
+function gebi(id) {
+  return document.getElementById(id);
 }
+var imgNames = [
+  'bird.png',
+  'plane.jpg',
+  'race.jpeg',
+  'mario.png',
+  'blue.jpg',
+  'eagle.jpeg',
+  'crocodile.png',
+  'game.jpg',
+  'sonic.png',
+  'car.jpg'
+];
+
+var images = [];
+
+var divImg = gebi('divImg');
+var divImg2 = gebi('divImg2');
+var divImg3 = gebi('divImg3');
+
+function getRandomInt() {
+  return Math.floor(Math.random() * imgNames.length);
+}
+
+function showNewImage(index, location){
+  console.log('index = ' + index);
+  console.log(location);
+  location.style['background-image'] = 'url("' + images[index].src + '")';
+  location.imageIndex = index;
+  images[index].incrementNshown();
+  var s = 'show counts: ';
+  images.map(function(pic){
+    s += pic.Nshown;
+    // console.log(pic);
+  });
+  // console.log(s);
+}
+
+function Image(src) {
+  this.src = 'img/' + src;
+  this.Nclicks = 0;
+  this.Nshown = 0;
+  this.incrementClicks = function() {
+    this.Nclicks++;
+  };
+  this.incrementNshown = function() {
+    this.Nshown++;
+  };
+}
+
+for (var i = 0; i < imgNames.length; i++) {
+  var img = new Image(imgNames[i]);
+  images.push(img);
+}
+
+// console.log('divImg.imageIndex = ' + divImg.imageIndex);
+
+divImg.addEventListener('click', function() {
+  refreshImage(divImg);
+});
+divImg2.addEventListener('click', function() {
+  refreshImage(divImg2);
+});
+divImg3.addEventListener('click', function() {
+  refreshImage(divImg3);
+});
+function refreshImage(location) {
+  images[location.imageIndex].incrementClicks();
+  var s = 'click counts: ';
+  images.map(function(ele) {s += ele.Nclicks;});
+  console.log(s);
+
+  showNewImage(getRandomInt(), divImg);
+  showNewImage(getRandomInt(), divImg2);
+  showNewImage(getRandomInt(), divImg3);
+}
+
+showNewImage(getRandomInt(), divImg);
+showNewImage(getRandomInt(), divImg2);
+showNewImage(getRandomInt(), divImg3);
+
+var displayVotesBtn = gebi('displayVotesBtn');
+displayVotesBtn.addEventListener('click', function(){
+  // display these as tables later?
+  gebi('totalVotes').innerHTML = 'here are your total # votes per image';
+});
